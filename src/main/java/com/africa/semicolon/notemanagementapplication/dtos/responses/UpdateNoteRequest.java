6eaf018b55ae8b4ee0986b6bd98a39946a3b3dbe
aondofe014704 <songujack@gmail.com>
2024-08-09 00:49:24 +0100

    update method added

---
+++ src/main/java/com/africa/semicolon/notemanagementapplication/dtos/responses/UpdateNoteRequest.java
@@ -0,0 +1,9 @@
+package com.africa.semicolon.notemanagementapplication.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UpdateNoteResponse {
+}
---
+++ src/main/java/com/africa/semicolon/notemanagementapplication/services/UpdateNoteResponse.java
@@ -0,0 +1,2 @@
+package com.africa.semicolon.notemanagementapplication.services;public class UpdateNoteResponse {
+}
